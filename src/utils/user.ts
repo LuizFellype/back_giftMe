1732004211112da34d88964f2488b5a353c44e58
Luiz Fellype <luiz.cassago@xdevel.com.br>
2019-03-19 00:25:01 -0300

    feat: add utils for user functionalites

---
+++ src/utils/user.ts
@@ -0,0 +1,23 @@
+import * as jwt from 'jsonwebtoken'
+import { Context } from '../interfaces'
+
+export const APP_SECRET = 'GraphQL-is-aw3some'
+
+export const uuid = (): string => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, char => {
+    let random = (Math.random() * 16) | 0
+    let value = char === 'x' ? random : (random % 4) + 8
+    return value.toString(16)
+  })
+}
+
+export const getUserId = (ctx: Context) => {
+  const authorization = ctx.request.get('Authorization')
+  if (authorization) {
+    const token = authorization.replace('Bearer ', '')
+    const userId = jwt.verify(token, APP_SECRET)
+    return userId
+  }
+
+  throw new Error('Not authenticated')
+}
