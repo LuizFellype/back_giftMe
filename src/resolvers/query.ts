9993698226abb24bb26e6ebe4195bf7a4578c278
Luiz Fellype <luiz.cassago@xdevel.com.br>
2019-04-21 08:14:50 -0300

    refactor: remove useless query

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -6,21 +6,7 @@ const query = {
     const user = getUserId(ctx)
     return ctx.db.query.user(
       { where: { id: user.userId } },
-      `{ id name recognizeId products { id productName url } partner { name recognizeId products { id productName url } } }`
-    )
-  },
-
-  feed: (parent: any, args: any, ctx: Context, info: any) => {
-    const id = args.filterId
-    if (!id) {
-      throw new Error('Must be a filterId pararmeter')
-    }
-
-    return ctx.db.query.products(
-      {
-        where: { postedBy: { id } }
-      },
-      `{ id productName url createdAt postedBy { name } }`
+      `{ id name email recognizeId products { id productName url } partner { name email recognizeId products { id productName url } } }`
     )
   }
 }
